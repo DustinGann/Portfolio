22efca85e674444711544d03daf6c81e32e331e3
DustinGann <123513919+DustinGann@users.noreply.github.com>
2025-11-14 19:06:55 +0000

    more prog

--- assets/js/scrypts.js
+++ assets/js/scrypts.js
@@ -8,6 +8,10 @@
       src: "assets/img/thumbnail/ttp.png",
       original: "assets/img/trash-taste-poster.png"
     },
+    {
+      src: "assets/img/thumbnail/gsit.png",
+      original: "assets/img/GSITimeline.png"
+    },
      {
       src: "assets/img/thumbnail/dv.jpg",
       original: "assets/img/man.png"
@@ -25,12 +29,13 @@
       original: "assets/img/MiiGunner2.png"
     },
      {
-      src: "assets/img/thumbnail/eye.jpg",
-      original: "assets/img/eye.gif"
+       src: "assets/img/thumbnail/nfl.jpg",
+      original: "assets/img/nifl.gif"
+      
     },
     {
-      src: "assets/img/thumbnail/nfl.jpg",
-      original: "assets/img/nifl.gif"
+     src: "assets/img/thumbnail/eye.jpg",
+      original: "assets/img/eye.gif"
     }
   ];
 
